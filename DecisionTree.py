27898af388e42a8a668ca1563fb9b30005c8654a
Eligijus <elasorama@gmail.com>
2021-04-07 18:33:14 +0300

    Initial version without recursion

--- DecisionTree.py
+++ DecisionTree.py
@@ -15,6 +15,10 @@ def GINI_impurity(y1_count: int, y2_count: int) -> float:
     # Getting the total observations
     n = y1_count + y2_count
     
+    # If n is 0 then we return the lowest possible gini impurity
+    if n == 0:
+        return 0.0
+
     # Getting the probability to see each of the classes
     p1 = y1_count / n
     p2 = y2_count / n
@@ -32,10 +36,8 @@ class Node:
     """
     def __init__(
         self, 
-        level: int, 
         Y: list 
     ):
-        self.level = level
         self.Y = Y 
 
         # Calculating the counts of Y in the node 
@@ -48,20 +50,26 @@ class Node:
         counts_sorted = list(sorted(self.counts.items(), key=lambda item: item[1]))
 
         # Getting the last item
-        yhat = counts_sorted[-1][0]
+        yhat = None
+        if len(counts_sorted) > 0:
+            yhat = counts_sorted[-1][0]
 
         # Saving to object attribute. This node will predict the class with the most frequent class
         self.yhat = yhat 
+
+        # Saving the number of observations in the node 
+        self.n = len(Y)
 
     def get_GINI(self):
         """
         Function to calculate the GINI impurity of a node 
         """
         # Getting the 0 and 1 counts
-        y1_count, y2_count = self.counts.get(0), self.counts.get(1)
+        y1_count, y2_count = self.counts.get(0, 0), self.counts.get(1, 0)
 
         # Getting the GINI impurity
         return GINI_impurity(y1_count, y2_count)
+
 
 
 class DecisionTree:
@@ -70,7 +78,7 @@ class DecisionTree:
         self, 
         max_depth: int,
         min_node_obs: int 
-        )
+        ):
         # Defining the maximum depth a tree can grow
         self.max_depth = max_depth
 
@@ -80,66 +88,71 @@ class DecisionTree:
         # When initializing the object, set the depth to 0 
         self.depth = 0 
 
-    @staticmethod
-    def GINI_impurity(y1_count, y2_count):
+    def best_split(self, X: pd.DataFrame, Y: list) -> tuple:
         """
-        Given the observations of a binary class calculate the GINI impurity
+        Given the X features and Y targets calculates the best split 
+        for a decision tree
         """
-        # Getting the total observations
-        n = y1_count + y2_count
-        
-        # Getting the probability to see each of the classes
-        p1 = y1_count / n
-        p2 = y2_count / n
-        
-        # Calculating GINI 
-        gini = 1 - (p1 ** 2 + p2 ** 2)
-        
-        # Returning the gini impurity
-        return gini
+        # Creating a dataset for spliting
+        df = X.copy()
+        df['Y'] = Y
 
-    def get_nodes(
-        self, 
-        X: pd.DataFrame,
-        Y: list
-        ):
-        """
-        Function that returns left and right nodes (two X and Y tuples)
-        """
-        # Saving the features
+        # Extracting all the features
         features = list(X.columns)
 
-        # Iterating through the features 
+        # Getting the GINI impurity for the base input 
+        counts = Counter(Y)
+        GINI_base = GINI_impurity(counts.get(0), counts.get(1))
 
-    @staticmethod
-    def GINI_impurity_df_split(
-        df:pd.DataFrame, 
-        target, 
-        split_X, 
-        split_value
-    ):
-        """
-        Function to calculate the resulting GINI impurity of a split
-        """
-        # Getting the left and right nodes
-        left = df[df[split_X] <= split_value]
-        right = df[df[split_X] > split_value]
-        
-        if (min_n > left.shape[0]):
-            return 0.5
-        
-        # Getting the counts and ginis
-        left_counts = left.groupby(target).size().values.tolist()
-        right_counts = right.groupby(target).size().values.tolist()
+        # Finding which split yields the best GINI gain 
+        max_gain = 0
 
-        gini_left = GINI_impurity(left_counts[0], left_counts[1])
-        gini_right = GINI_impurity(right_counts[0], right_counts[1])
-        
-        # Getting the final weighted GINI impurity
-        w1 = left.shape[0]/ (left.shape[0] + right.shape[0]) 
-        w2 = right.shape[0] / (left.shape[0] + right.shape[0]) 
-        
-        return w1 * gini_left + w2 * gini_right
+        # Default best feature and split
+        best_feature = features[0]
+        best_value = 0
+
+        for feature in features:
+            # Droping missing values
+            Xvalues = X[feature].dropna()
+
+            # Sorting the values and getting the rolling average
+            Xvalues = Xvalues.sort_values().rolling(2).mean()
+
+            # Converting to list
+            Xvalues = Xvalues.values.tolist()
+            
+            # Droping the initial NaN value 
+            Xvalues.pop(0)
+
+            for value in Xvalues:
+                # Spliting the dataset 
+                left_df = df[df[feature]<value]
+                right_df = df[df[feature]>=value]
+
+                # Creating the two nodes 
+                left_node = Node(left_df['Y'].values.tolist())
+                right_node = Node(right_df['Y'].values.tolist())
+
+                # Calculating the weights for each of the nodes
+                w_left = left_node.n / (left_node.n + right_node.n)
+                w_right = right_node.n / (left_node.n + right_node.n)
+
+                # Calculating the weighted GINI impurity
+                wGINI = w_left * left_node.gini_impurity + w_right * right_node.gini_impurity
+
+                # Calculating the GINI gain 
+                GINIgain = GINI_base - wGINI
+
+                # Checking if this is the best split so far 
+                if GINIgain > max_gain:
+                    best_feature = feature
+                    best_value = value 
+
+                    # Setting the best gain to the current one 
+                    max_gain = GINIgain
+
+        return (best_feature, best_value)
+
 
 
 if __name__ == '__main__':
@@ -151,4 +164,9 @@ if __name__ == '__main__':
     Y = d['Survived'].values.tolist()
 
     # Creating the node object 
-    node = Node(level=0, Y=Y)
+    DT = DecisionTree(max_depth=2, min_node_obs=5)
+
+    # Gettting the best split
+    (feature, split) = DT.best_split(X, Y)
+
+    print((feature, split))
